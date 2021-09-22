f97103d268b250daf6d9d5ea3c8ac02d7c4a7c1c
SoftwareSing <softwaresing@gmail.com>
2021-09-22 23:45:38 +0800

    新增註解方便測試
    在註解加上算完 sha 後的值

--- dev-script/upsertTestScCard.js
+++ dev-script/upsertTestScCard.js
@@ -17,18 +17,21 @@ async function upsertTestScCard () {
     goodThru: '4321',
     code: '789',
     password: crypto.createHash('sha3-256').update('xyzabc').digest('hex')
+    // 04ac8358eacbd447ff99ac1b893f39d6518143e131ab1a33ea66e01a0905f0e8
   })
   await ScCardRepo.upsertScCard({
     scCardNo: '4444555566667777',
     goodThru: '4321',
     code: '789',
     password: crypto.createHash('sha3-256').update('abcxyz').digest('hex')
+    // b90674a7b7d345236d7d500905d8e8075ab15d5f4b4fd8fb3e0e7e42ec52133e
   })
   await ScCardRepo.upsertScCard({
     scCardNo: '1111333355557777',
     goodThru: '5678',
     code: '246',
     password: crypto.createHash('sha3-256').update('qwerasdf').digest('hex')
+    // a7a30ebd1806386903f253af20b4132891c446f202249039f874ab12c24ce69a
   })
 }
 
