3d2a2791f10a130ebc13ab564d03f74224ba6297
SoftwareSing <softwaresing@gmail.com>
2021-09-22 23:21:02 +0800

    將 password hash 切換為 argon2id 算法

--- src/entity/scCard/ScCard.js
+++ src/entity/scCard/ScCard.js
@@ -18,7 +18,7 @@ module.exports = class ScCard {
   }
 
   static hashPasswordByArgon2 (password) {
-    return argon2.hash(password)
+    return argon2.hash(password, { type: argon2.argon2id })
   }
 
   verifyPassword (password) {
