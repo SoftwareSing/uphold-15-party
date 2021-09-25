ab69b4923fbdc643ba45b4821603e36fa1a31460
SoftwareSing <softwaresing@gmail.com>
2021-09-25 19:31:16 +0800

    Update syncIndex.js

--- dev-script/syncIndex.js
+++ dev-script/syncIndex.js
@@ -1,6 +1,7 @@
 const Mongodb = require('~common/connection/Mongodb')
 const ScCardModel = require('~entity/scCard/ScCardModel')
 const ScOrderModel = require('~entity/scOrder/ScOrderModel')
+const ScOrderLogModel = require('~entity/scOrderLog/ScOrderLogModel')
 
 async function run () {
   await Mongodb.connect()
@@ -9,7 +10,7 @@ async function run () {
 }
 
 async function syncIndex () {
-  for (const model of [ScCardModel, ScOrderModel]) {
+  for (const model of [ScCardModel, ScOrderModel, ScOrderLogModel]) {
     await model.syncIndexes()
   }
 }
